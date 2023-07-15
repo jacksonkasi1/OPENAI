38648567cacfc1d4576ae8001914ea8cd0068057
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2023-07-15 22:06:09 +0530

    update comments

--- apps/server/train/generate-training-data.js
+++ apps/server/train/generate-training-data.js
@@ -1,24 +1,3 @@
-// const { openai } = require("../openai");
-
 const { success, info } = require("../utils/debug");
 
-// // ** import debug
-// const error = require("debug")("error");
-// const info = require("debug")("info");
-
-// error("error");
-
-// const generateJsonl = async () => {
-//   try {
-//   } catch (error) {
-//     error(error);
-//   }
-// };
-
-(async () => {
-  info("work start...");
-  success("2 sec delay");
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  success("2 sec delay");
-})();
-
+const { openai } = require("../openai");
